325c97088f5da32bb9d4bd947748b3ad217af157
brn.lin <brn.lin@gmail.com>
2026-04-30 09:47:44 -0700

    Added axios interceptor for server responses to handle expired/invalid tokens.

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -6,6 +6,7 @@ const api = axios.create({
   baseURL: API_URL,
 });
 
+// Attach token
 api.interceptors.request.use((config) => {
   const token = sessionStorage.getItem("token");
 
@@ -17,5 +18,18 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
+
+// Handle expired/invalid token
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      sessionStorage.removeItem("token");
+      window.location.href = "/";
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default api;
