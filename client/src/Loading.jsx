16456586459412e2fab5071ea3b96f3921a59302
brn.lin <brn.lin@gmail.com>
2026-05-05 11:21:24 -0700

    Added a loading state to editor layout.

---
+++ client/src/Loading.jsx
@@ -0,0 +1,11 @@
+import "./Loading.css";
+
+function Loading() {
+  return (
+    <div className="loading__container">
+      <div className="loading__spinner" />
+    </div>
+  );
+}
+
+export default Loading;
--- client/src/app-components/editor-layout/EditorLayout.jsx
+++ client/src/app-components/editor-layout/EditorLayout.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Sidebar from "../sidebar/Sidebar";
 import CenterEditor from "../center-editor/CenterEditor";
+import { useWorkspace } from "../../context/WorkspaceContext";
+import Loading from "../../Loading";
 import "./EditorLayout.css";
 
 const EditorLayout = () => {
+  const { workspaceId } = useWorkspace();
+
+  if (!workspaceId) {
+    return <Loading />;
+  }
+
   return (
     <div className="editor-layout">
       <Sidebar />
