abdb3d45e9744b0e1481e3ee68a535c5d5903ee9
brn.lin <brn.lin@gmail.com>
2026-05-22 09:49:02 -0700

    Made sure local backend connects to local database and deployed backend connects to deployed database.

--- server/db.js
+++ server/db.js
@@ -1,20 +1,11 @@
 const { Pool } = require("pg");
 
-const pool = new Pool(
-  process.env.DATABASE_URL
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }
-    : {
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-      },
-);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+});
 
 module.exports = pool;
