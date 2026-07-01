1bb9540a286313640bf108f559f48ff01932cc26
brn.lin <brn.lin@gmail.com>
2026-07-01 13:32:15 -0700

    Turend the Item type into a discriminated union, to separate between pages and blocks.

--- client/src/types/item.ts
+++ client/src/types/item.ts
@@ -1,19 +1,35 @@
-export type ItemType = "page" | "block";
-
-export type Item = {
+export type BaseItem = {
   id: string;
   workspace_id: string;
   parent_id: string | null;
-  type: ItemType;
-  title: string | null;
-  content: Record<string, unknown>;
   position: number;
   created_by: string | null;
+  updated_by: string | null;
   created_at: string;
   updated_at: string;
   deleted_at: string | null;
   trashed_by_id: string | null;
 };
+
+export type PageContent = {};
+
+export type BlockContent = {
+  text: string;
+};
+
+export type PageItemType = BaseItem & {
+  type: "page";
+  title: string | null;
+  content: PageContent;
+};
+
+export type BlockItemType = BaseItem & {
+  type: "block";
+  title: null;
+  content: BlockContent;
+};
+
+export type Item = PageItemType | BlockItemType;
 
 export type TrashedItem = Item & {
   deleted_at: string; // deleted_at is always a string in Trash
