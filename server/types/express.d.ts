bf99e69ed073a07f6440264b0c1b2807686dc9aa
brn.lin <brn.lin@gmail.com>
2026-07-06 14:43:01 -0700

    Created a declaration file to globally add the AuthTokenPayload type to Express's Request type so that TypeScript knows that a user property exists.

---
+++ server/types/express.d.ts
@@ -0,0 +1,11 @@
+import type { AuthTokenPayload } from "../middleware/authMiddleware.js";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: AuthTokenPayload;
+    }
+  }
+}
+
+export {};
