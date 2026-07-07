e2372e2df1050046945f6306f58ba6b179a25fb3
brn.lin <brn.lin@gmail.com>
2026-07-07 14:41:57 -0700

    Refactored demoRoutes.ts.

--- server/routes/demoRoutes.ts
+++ server/routes/demoRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-const router = express.Router();
 
 import { authMiddleware } from "../middleware/authMiddleware.js";
 import { resetDemoController } from "../controllers/demoController.js";
+
+const router = express.Router();
 
 // ------------------
 // Reset Demo workspace
