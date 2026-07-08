9aaabb285816168deefc8138f1ddf77e3d76cbfa
brn.lin <brn.lin@gmail.com>
2026-07-08 13:07:22 -0700

    Added workspaceId and memberRole properties to Express's global Request type.

--- server/types/express.d.ts
+++ server/types/express.d.ts
@@ -1,10 +1,12 @@
 import type { AuthTokenPayload } from "../middleware/authMiddleware.js";
+import type { Role } from "../utils/permissions.js";
 
 declare global {
   namespace Express {
     interface Request {
       user?: AuthTokenPayload;
-      workspaceId: string;
+      workspaceId?: string;
+      memberRole?: Role;
     }
   }
 }
