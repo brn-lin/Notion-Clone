c7612104683ba973a08ae48a3460bfac86e95d99
brn.lin <brn.lin@gmail.com>
2026-08-11 10:15:23 -0700

    Added another local dev server URL to allowed CORS origins.

--- server/index.ts
+++ server/index.ts
@@ -22,6 +22,7 @@ const PORT = Number(process.env.PORT) || 5000;
 app.use(
   cors({
     origin: [
+      "http://127.0.0.1:5173",
       "http://localhost:5173",
       "https://notion-clone-lilac-two.vercel.app",
     ],
